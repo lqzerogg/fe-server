8d5cbf592010888a5687f5cfd5035c63419dac74
wangshishuo <wangshishuo@gmail.com>
2013-06-25 02:08:03 -0400

    init

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,30 @@ var lastMinute
 exports.index = function(req, res, next) {
 	res.set('Content-Type', 'image/jpeg')
 	res.send(200)
-	var now = new Date()
-		, minTime = now.getFullYear() + '/' + (now.getMonth()+1) + '/' + now.getDate()
-			+ ' ' + now.getHours() + ':' + now.getMinutes() + ':00'
+	var minuteTime = parseMinuteTime(new Date())
 		, result = parseReq(req)
-	if (lastMinute === minTime) {
-		mc.append(minTime, JSON.stringify(result) + ',')
+	if (lastMinute === minuteTime) {
+		mc.append(minuteTime, JSON.stringify(result) + ',', function(err, response) {
+			console.log(err === null ? minuteTime + ' updated' : err)
+		})
 	} else {
-		lastMinute = minTime;
-		mc.set(minTime, JSON.stringify(result) + ',', 120) // keep 2 minutes
-		console.log(minTime)
+		lastMinute = minuteTime;
+		mc.set(minuteTime, JSON.stringify(result) + ',', function(err, response) {
+			console.log(err === null ? minuteTime + ' set' : err)
+		}, 120) // keep 2 minutes
 	}
+}
+
+exports.get = function(req, res, next) {
+	var minuteTime
+	if (typeof req.query.time !== 'undefined' && req.query.time !== null) {
+		minuteTime = req.query.time
+	} else {
+		minuteTime = parseMinuteTime(new Date())
+	}
+	mc.get(minuteTime, function(err, result) {
+		res.send(result)
+	})
 }
 
 function parseReq(req) {
@@ -28,4 +41,10 @@ function parseReq(req) {
 	result.domReady   = parseInt(req.query.domContentLoadedEventStart) - parseInt(req.query.fetchStart)
 	result.load       = parseInt(req.query.loadEventStart) - parseInt(req.query.fetchStart)
 	return result
+}
+
+function parseMinuteTime(date) {
+	return date.getFullYear() + '/' + ('0'+(date.getMonth()+1)).slice(-2) + '/' + ('0'+date.getDate()).slice(-2)
+			+ ('0'+date.getHours()).slice(-2) + ':' + ('0'+date.getMinutes()).slice(-2) + ':00'
+
 }
